6a88d147ebc6ddf428b85e9c5f661ca256a1c34c
John Familoye <fajob20@yahoo.com>
2023-07-02 08:57:16 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	int j;
@@ -18,4 +18,3 @@ void print_alphabet_x10(void);
 		_putchar('\n');
 	}
 }
-
