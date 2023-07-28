c31819c641e820c15ce963a973b48baf5fb767b9
John Familoye <fajob20@yahoo.com>
2023-07-27 17:43:05 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+  * free_list - frees a linked list
+  * @head: the beginning of the list
+  */
+
+void free_list(list_t *head)
+{
+	list_t* temp
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
