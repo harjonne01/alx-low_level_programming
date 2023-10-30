876b0a92a54a9a3f04081c60f3d8327f0fb1362b
John Familoye <fajob20@yahoo.com>
2023-10-30 15:49:20 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to be created
+ * @text_content: NULL terminated string to write to the file
+ *
+ *
+ * Return: 1, on success and -1, on failure
+ * or -1 if the filename is NULL
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int length_text;
+	ssize_t written_bytes;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	length_text = 0;
+	while (text_content[length_text] != '\0')
+	{
+		length_text++;
+	}
+
+	written_bytes = write(fd, text_content, length_text);
+	if (written_bytes < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
