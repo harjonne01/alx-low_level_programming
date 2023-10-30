8df009077540262f9bcf0cbb9d2291a3a2e67d75
John Familoye <fajob20@yahoo.com>
2023-10-30 15:52:06 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	length_text = 0;
-	while (text_content[length_text] != '\0')
+	while (text_content[length_text] != NULL)
 	{
 		length_text++;
 	}
