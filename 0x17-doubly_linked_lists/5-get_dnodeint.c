8149edab1d7af320c56e2e8a9a4edabcb240051e
John Familoye <fajob20@yahoo.com>
2023-12-10 10:21:30 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,3 +6,23 @@
  * @index: index of the node to return
  * Return: address of the node, or if it does not exist, NULL
  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int num = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head != NULL)
+	{
+		if (num == index)
+		{
+			return (head);
+		}
+
+		num++;
+		head = head->next;
+	}
+
+	return (NULL);
+}
