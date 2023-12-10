9910bf2e8a10f89d8bb8fea19784af9aa87cadd5
John Familoye <fajob20@yahoo.com>
2023-12-10 10:06:59 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,3 +5,15 @@
  * @head: pointer to the beginning of the linked list
  * Return: void
  */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
